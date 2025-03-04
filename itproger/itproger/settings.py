8a32b22426e77f6d175588183094616f82ee75a3
alan51532 <alan5@mail.ru>
2025-03-04 07:44:39 +0300

    Добавлены статические файл2

--- itproger/itproger/settings.py
+++ itproger/itproger/settings.py
@@ -75,9 +75,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
+STATICFILES_DIRS = []
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
